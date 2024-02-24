d601a0e2047e6e9da975075d43877f0ea5f1c9da
GpAndrad2 <123668518+GpAndrad2@users.noreply.github.com>
2024-02-23 22:01:28 -0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,21 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>CENA I  Shrek: era uma vez, um pequeno ogro chamado Shrek, no qual, vivia numa arvore com seus parentes, era um lugar pequeno e nojento, mas ele era feliz, pois ogros gostam de lugares assim, no dia de seu aniversário de sete anos, sua mãe o chamou para uma conversa, no qual é uma tradição, e todos os pequenos ogros deviam seguir.Mamãe ogro: Filho meu que cresce tão depressaCresce mais e não vai pararA hora vem aiÀ hora de partirSete anosSete anosJá ta bom pra se virarMamãe fez sanduíche pra vocêtambém pus cobertor, pois vai nevarOrgulho dos seus paisJamais olhar pra trásSempre em frenteSempre em frenteVocê vai, vai se encontrarÉ um mundo grande e felizCom céu azul sobre nósA vida é legal e faz bilu-biluMas não pra tuÉ um mundo grande e felizE tudo é diversão, tudo é corAmigos pra brincarFesta pra chuchuMas não pra tuFilhinho meuMeu bem a gente é feioE o povo então.Não quer olhar pra nósE toma palavrãoE toma tropeçãoVem pra nos caçarE calam nossa vozSendo assim você precisa achar seu cantoAlgum lugar difícil de encontrar
+Porém se alguém chegarVocê tem que assustar bemBota medoBota medoSó assim pra não dançarAdeus!Cuidado com os homens maus (Shrek passa em lugares e assusta moradores)***Shrek:Eu não quero um mundo felizJá sou feliz aqui, como eu souCom tudo que é meuPortanto vejam bemEu não quero um mundo felizNa festa mando eu e só euFaço o que eu quiserEu convido e vou tambémPegue o mundo azul de vocêsCom ursinhos e unicórniosE enfie num lugarBem fundo e bem quentinho, simEu prefiro a vida assimNão sou tão complicadoEu nasci pra ser sozinhoE pra sempre detestadoTodo livro fala em mimComo um degeneradoMas tudo bemPois tanto fazcaguei pra ser amadoCaguei pro mundo belo e felizEu tenho o meu, meu mundinho só meuQue não é belo e nem é felizMas é meu!É todo meu!Ele é meu!Ele é meu</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 4,
+    backgroundColor: 'green',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
